55156eea059d14e2c4933a70ee883f1a6755ab33
Jay Medina <jay.medina.celis@gmail.com>
2023-02-07 14:44:12 -0500

    Displays correct letters in corresponding spaces

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 secretWord = "happy"
 
 counter = 6
+for i in secretWord:
+    print("_", end = ' ')
+print('')
+
+correctLetters = []
 
 while counter > 0:
-    for i in secretWord:
-        print("_", end = ' ')
-    print('')
+   
     player = input("Geuss a letter: ")
     
     if player in secretWord:
-        print("you guessed correctly")
+        print("You guessed correctly")
+        correctLetters.append(player)
+        print("correct list:", correctLetters)
+        for letter in secretWord:
+            if letter in correctLetters: # and letter == :
+                print(letter, end = ' ')
+            else:
+                print("_", end = ' ')
+        print('')
+   
     else: 
         counter -= 1
         print(f"Number of guesses left: {counter}")
